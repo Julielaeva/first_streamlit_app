54a156e5593e4064e3602a46e40e3753899f0d49
Yuliya <70970950+Julielaeva@users.noreply.github.com>
2022-11-21 13:13:17 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My Mom's new healthy dinner menu")
-streamlit.header('Breakfast Menu')
+streamlit.header('🥗🐔 Breakfast Favorites 🥑🍞')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
