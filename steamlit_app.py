43f9b0e52f3b9ca8f2b5f22ff68f42cb4c41da08
Yuliya <70970950+Julielaeva@users.noreply.github.com>
2022-11-24 16:50:15 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -29,5 +29,5 @@ streamlit.text(fruityvice_response.json())
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#streamlit.dataframe(fruityvice_normalized)
+pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
