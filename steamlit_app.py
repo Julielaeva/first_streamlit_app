818e846847aa3787cdf0f180c80db8ed3f838d73
Yuliya <70970950+Julielaeva@users.noreply.github.com>
2022-11-24 16:51:14 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -27,7 +27,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-
+#from nested semi structured jsot to a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-pandas.json_normalize(fruityvice_response.json())
+
 streamlit.dataframe(fruityvice_normalized)
