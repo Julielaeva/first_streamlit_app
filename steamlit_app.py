35e2f0959cad4c2af1e047ecd0caebe53ec32607
Yuliya <70970950+Julielaeva@users.noreply.github.com>
2022-11-25 17:42:52 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -34,8 +34,8 @@ streamlit.dataframe(fruits_to_show)
 #From the fruitvise website
 
 #creating function
-def get_fruitvise_data(this_fruit choice):
- fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+def get_fruitvise_data(this_fruit_choice):
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #from nested semi structured json to a flat table
  return fruityvice_normalized
   
@@ -44,7 +44,7 @@ def get_fruitvise_data(this_fruit choice):
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
- fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
  if not fruit_choice:
   streamlit.error('Please select a fruit to get information')
  else:
